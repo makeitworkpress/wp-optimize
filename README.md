db6d85662ffb7af8de370b8e16a70345eb71056c
Michiel <michielsite@hotmail.com>
2017-12-21 12:49:03 +0100

    Renamed functions and added some new functions!

--- README.md
+++ README.md
@@ -11,71 +11,30 @@ A true value will execute the given optimisation.
 Currently, the class defaults to the following optimisations:
 
             $optimisations = array(
-                'blockExternalHTTP'         => false,
-                'deferCSS'                  => false,
-                'deferJS'                   => false,
-                'disableEmbed'              => false,
-                'disableComments'           => false,
-                'disableRestApi'            => false,
-                'disableXMLRPC'             => true,
-                'jqueryToFooter'            => true,
-                'removeEmoji'               => true,
-                'removeFeeds'               => false,
-                'removeHeartbeat'           => false,
-                'removejQuery'              => false,
-                'removeShortlinks'          => true,
-                'removeVersionNumbers'      => true,            
-                'removeWLWManifest'         => true,
-                'removeWPVersion'           => true
+                'blockExternalHTTP'         => false, // Block requests to external http on the front-end side. Thus, blocks all request that are done by plugins to external addresses.
+                'deferCSS'                  => false, // Adds defer="defer" to all enqueued JavaScript files.
+                'deferJS'                   => true,  // Defers all registered scripts using the loadCSS function from the Filament Group.  
+                'disableComments'           => false, // Disables the comments functionality and removes it from the admin menu.
+                'disableEmbed'              => false, // Removes the script files that are enqueued by the WordPress media embed system.
+                'disableEmoji'              => true,  // Removes the scripts that are enqueued for displaying emojis.
+                'disableFeeds'              => false, // Removes the post feeds.
+                'disableHeartbeat'          => false, // Unregisters the heartbeat scripts, which is usually responsible for autosaves.
+                'disablejQuery'             => false, // Removes the default jQuery script.
+                'disablejQueryMigrate'      => true,  // Removes the jQuery Migrate script.
+                'disableRestApi'            => false, // Disables the rest api.
+                'disableRSD'                => true,  // Removes the RDS link in the head section of the site.
+                'disableShortlinks'         => true,  // Removes the shortlinks in the head section of the site.                     
+                'disableVersionNumbers'     => true,  // Removes the version trail in enqueued scripts and styles.           
+                'disableWLWManifest'        => true,  // Removes the WLW Manifest links in the head section of the site.
+                'disableWPVersion'          => true,  // Removes the WP version from the head section of the site.           
+                'disableXMLRPC'             => true,  // Disables the xmlrpc functionality.
+                'jqueryToFooter'            => true,  // Moves the default jQuery script to the footer.
+                'limitCommentsJS'           => true,  // Limits the JS for comments only to singular entities
+                'limitRevisions'            => true,  // Limits the number of revisions to 5
+                'removeCommentsStyle'       => true,  // Removes the .recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;} styling in the head section
+                'slowHeartbeat'             => true   // Slows the heartbeat down to one per minute
             );
-            
-**removeVersionNumbers (boolean)**
-Removes the version trail in enqueued scripts and styles.
-
-**removeWPVersion (boolean)**
-Removes the WP version from the head section of the site.
-
-**removeFeeds (boolean)**
-Removes the post feeds.
-
-**removeShortlinks (boolean)**
-Removes the shortlinks in the head section of the site.
-
-**removeWLWManifest (boolean)**
-Removes the WLW Manifest links in the head section of the site.
-
-**removeEmoji (boolean)**
-Removes the scripts that are enqueued for displaying emojis.
-
-**disableXMLRPC (boolean)**
-Disables the xmlrpc functionality.
-
-**blockExternalHTTP (boolean)**
-Block requests to external http on the front-end side. Thus, blocks all request that are done by plugins to external addresses.
-
-**removeHeartbeat (boolean)**
-Unregisters the heartbeat scripts, which is usually responsible for autosaves.
-
-**disableComments (boolean)**
-Disables the comments functionality and removes it from the admin menu.
-
-**disableRestApi (boolean)**
-Disables the rest api.
-
-**removejQuery (boolean)**
-Removes the default jQuery script.
-
-**jqueryToFooter (boolean)**
-Moves the default jQuery script to the footer.
-
-**disableEmbed (boolean)**
-Removes the script files that are enqueued by the WordPress media embed system.
-
-**deferJS (boolean)**
-Adds defer="defer" to all enqueued JavaScript files.
-
-**deferCSS (boolean)**
-Defers all registered scripts using the loadCSS function from the Filament Group.     
+               
 
 ### Create instance
 Create a new instance of the WP_Optimize class with your optimisations array as arguments.
--- optimize.php
+++ optimize.php
@@ -2,7 +2,7 @@
 /**
  * Class wrapper for removing unnecessary WordPress functions
  *
- * @author Michiel Tramper - https://www.michieltramper.com & https://www.makeitworkpress.com
+ * @author Michiel Tramper, https://www.makeitworkpress.com
  */
 namespace WP_Optimize;
 use WP_Error as WP_Error;
@@ -21,26 +21,36 @@ class Optimize {
      *
      * @param array $optimize The optimalizations to conduct
      */
-    public function __construct(Array $optimizations = array()) {
+    public function __construct( Array $optimizations = array() ) {
+
         $defaults =  array(
             'blockExternalHTTP'         => false,
-            'disableEmbed'              => false,
+            'deferCSS'                  => false,
+            'deferJS'                   => true,
             'disableComments'           => false,
+            'disableEmbed'              => false,
+            'disableEmoji'              => true,
+            'disableFeeds'              => false,
+            'disableHeartbeat'          => false,
+            'disablejQuery'             => false,
+            'disablejQueryMigrate'      => true,
             'disableRestApi'            => false,
+            'disableRSD'                => true,
+            'disableShortlinks'         => true,                       
+            'disableVersionNumbers'     => true,            
+            'disableWLWManifest'        => true,
+            'disableWPVersion'          => true,            
             'disableXMLRPC'             => true,
             'jqueryToFooter'            => true,
-            'removeEmoji'               => true,
-            'removeFeeds'               => false,
-            'removeHeartbeat'           => false,
-            'removejQuery'              => false,
-            'removeShortlinks'          => true,
-            'removeVersionNumbers'      => true,            
-            'removeWLWManifest'         => true,
-            'removeWPVersion'           => true
+            'limitCommentsJS'           => true,
+            'limitRevisions'            => true,
+            'removeCommentsStyle'       => true,
+            'slowHeartbeat'             => true
         );
         
         $this->optimize = wp_parse_args($optimizations, $defaults);
         $this->optimize();
+
     }
     
     /**
@@ -68,9 +78,78 @@ class Optimize {
             }, 100 );
         }
     }
+
+    /**
+     * Defers all CSS using loadCSS from the Filament Group. Thanks dudes and dudettes!
+     */
+    private function deferCSS() {
+        
+        // Rewrite our object context
+        $object = $this;
+        
+        // Dequeue our CSS and save our styles. Please note - this function removes conditional styles for older browsers
+        add_action( 'wp_enqueue_scripts', function() use( $object ) {
+            
+            global $wp_styles;
+            
+            // Save the queued styles
+            foreach( $wp_styles->queue as $style ) {    
+                $object->styles[]   = $wp_styles->registered[$style];  
+                $dependencies       = $wp_styles->registered[$style]->deps;
+                
+                if( ! $dependencies) {
+                    continue;
+                }
+                
+                    // Add dependencies, but only if they are not included yet
+                foreach( $dependencies as $dependency ) { 
+                    $object->styles[] = $wp_styles->registered[$dependency];
+                }  
+
+            }
+            
+            // Remove duplicate values because of the dependencies
+            $object->styles = array_unique( $object->styles, SORT_REGULAR );
+
+            // Dequeue styles and their dependencies except for conditionals
+            foreach( $object->styles as $style ) {
+                wp_dequeue_style($style->handle);
+            }            
+            
+        }, 9999);        
+        
+        // Load our CSS using loadCSS
+        add_action( 'wp_head', function() use( $object ) {
+         
+            $output = '<script>function loadCSS(a,b,c,d){"use strict";var e=window.document.createElement("link"),f=b||window.document.getElementsByTagName("script")[0],g=window.document.styleSheets;return e.rel="stylesheet",e.href=a,e.media="only x",d&&(e.onload=d),f.parentNode.insertBefore(e,f),e.onloadcssdefined=function(b){for(var c,d=0;d<g.length;d++)g[d].href&&g[d].href.indexOf(a)>-1&&(c=!0);c?b():setTimeout(function(){e.onloadcssdefined(b)})},e.onloadcssdefined(function(){e.media=c||"all"}),e}';
+            foreach( $object->styles as $style ) { 
+                if( isset($style->extra['conditional'] ) ) 
+                    continue;
+                
+                // Load local assets
+                if( strpos($style->src, 'http') === false )    
+                    $style->src = site_url() . $style->src;
+                $output .= 'loadCSS("' . $style->src . '", "", "' . $style->args . '");';           
+            }
+            $output .= '</script>';
+            
+            echo $output;
+            
+        }, 9999);
+        
+    }    
     
     /**
-     * Removes the support and appearance of comments
+     * Defers all JS
+     */
+    private function deferJS() {
+        add_filter( 'script_loader_tag', function( $tag ) {
+            return str_replace( ' src', ' defer="defer" src', $tag );    
+        }, 10, 1 );    
+    }    
+    
+    /**
+     * Disables the support and appearance of comments
      */  
     private function disableComments() {
         
@@ -83,7 +162,7 @@ class Optimize {
         add_filter( 'comments_open', '__return_false', 20, 2 );
         add_filter( 'pings_open', '__return_false', 20, 2 );
         
-        // Removes admin support for post types and menus
+        // Disables admin support for post types and menus
         add_action( 'admin_init', function() {
             
             $post_types     = get_post_types();
@@ -115,19 +194,24 @@ class Optimize {
      * Removes the Embed Javascript and References        
      */    
     private function disableEmbed() {
+
+        add_action( 'wp_enqueue_scripts', function() {
+            wp_deregister_script('wp-embed');
+        }, 100 );
+
         add_action( 'init', function() {
         
             // Removes the oEmbed JavaScript.
-            remove_action('wp_head', 'wp_oembed_add_host_js'); 
+            remove_action( 'wp_head', 'wp_oembed_add_host_js' ); 
 
             // Removes the oEmbed discovery links.
-            remove_action('wp_head', 'wp_oembed_add_discovery_links');        
+            remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );        
 
             // Remove the oEmbed route for the REST API epoint.
-            remove_action('rest_api_init', 'wp_oembed_register_route');
+            remove_action( 'rest_api_init', 'wp_oembed_register_route' );
 
             // Disables oEmbed auto discovery.
-            remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
+            remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
             
             // Turn off oEmbed auto discovery.
             add_filter( 'embed_oembed_discover', '__return_false' );            
@@ -141,14 +225,155 @@ class Optimize {
      */
     private function disableRestApi() {
         
-        // Remove the REST API lines from the HTML Header
+        // Remove the references to the JSON api
         remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+        remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+        remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+        add_filter( 'embed_oembed_discover', '__return_false' );
+        remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+        remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+        remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+        remove_action( 'template_redirect', 'rest_output_link_header', 11, 0 );
 
-        // Filters for WP-API version 2.x
+
+        // Disable the API completely
+        add_filter('json_enabled', '__return_false');
+        add_filter('json_jsonp_enabled', '__return_false');
         add_filter('rest_enabled', '__return_false');
-        add_filter('rest_jsonp_enabled', '__return_false');        
+        add_filter('rest_jsonp_enabled', '__return_false');      
+        
+    }     
+         
+    /**
+     * Removes WP Emoji
+     */
+    private function disableEmoji() {
+        remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+        remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+        remove_action( 'admin_print_styles', 'print_emoji_styles' );
+        remove_action( 'wp_print_styles', 'print_emoji_styles' );
+        remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+        remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+        remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+        
+        /**
+         * Removes Emoji from the TinyMCE Editor
+         *
+         * @param array $plugins The plugins hooked onto the TinyMCE Editor
+         */
+        add_filter( 'tiny_mce_plugins', function( $plugins ) {
+            if ( ! is_array($plugins) ) {
+                return array();
+            }
+            return array_diff($plugins, array('wpemoji'));            
+        }, 10, 1 );       
+    }    
+      
+    /**
+     * Removes links to RSS feeds
+     */
+    private function disableFeeds() {        
+        remove_action( 'wp_head', 'feed_links_extra', 3 ); 
+        remove_action( 'wp_head', 'feed_links', 2 );   
+        add_action( 'do_feed', array($this, 'disableFeedsHook'), 1 );
+        add_action( 'do_feed_rdf', array($this, 'disableFeedsHook'), 1 );
+        add_action( 'do_feed_rss', array($this, 'disableFeedsHook'), 1 );
+        add_action( 'do_feed_rss2', array($this, 'disableFeedsHook'), 1 );
+        add_action( 'do_feed_atom', array($this, 'disableFeedsHook'), 1 );        
+    }  
+    
+    /**
+     * Removes the actual feed links
+     */
+    public function disableFeedsHook() {
+        wp_die( '<p>' . __('Feed disabled by WP Optimize.') . '</p>' );
+    }
+    
+    /**
+     * Removes the WP Heartbeat Api. Caution: this disables the autosave functionality 
+     */
+    private function disableHeartbeat() {
+        add_action('admin_enqueue_scripts', function() {
+            wp_deregister_script('heartbeat');    
+        });
+    }
+    
+    /**
+     * Deregisters jQuery.
+     */    
+    private function disablejQuery() {
+        add_action( 'wp_enqueue_scripts', function() {
+            wp_deregister_script('jquery');
+        }, 100 );
+    }     
+
+    /**
+     * Deregisters jQuery Migrate by removing the dependency.
+     */    
+    private function disablejQueryMigrate() {
+
+        add_filter( 'wp_default_scripts', function( $scripts ) {
+            if( ! empty($scripts->registered['jquery']) ) {
+                $scripts->registered['jquery']->deps = array_diff( $scripts->registered['jquery']->deps, array( 'jquery-migrate' ) );
+            }
+        } );
+
+    }
+  
+    /**
+     * Disables RSD Links, used by pingbacks
+     */
+    private function disableRSD() { 
+        remove_action('wp_head', 'rsd_link'); 
+    }     
+    
+    /**
+     * Removes the WP Shortlink 
+     */
+    private function disableShortlinks() { 
+        remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );        
+    }    
+    
+    /**
+     * Removes the version hook on scripts and styles
+     *
+     * @uses MT_WP_Optimize::no_scripts_styles_version_hook
+     */
+    private function disableVersionNumbers() {
+        add_filter( 'style_loader_src', array($this, 'disableVersionNumbersHook'), 9999 );
+        add_filter( 'script_loader_src', array($this, 'disableVersionNumbersHook'), 9999 ); 
+    }
+    
+    /**
+     * Removes version numbers from scripts and styles. 
+     * The absence of version numbers increases the likelyhood of scripts and styles being cached.
+     *
+     * @param string @target_url The url of the script
+     */
+    public function disableVersionNumbersHook( $target_url = '' ) {
+        
+        if( strpos( $target_url, 'ver=' ) ) {
+            $target_url = remove_query_arg( 'ver', $target_url );
+        }
+        
+        return $target_url;
         
     }
+    
+    /**
+     * Removes WLW manifest bloat
+     */
+    private function disableWLWManifest() {
+        remove_action('wp_head', 'wlwmanifest_link');   
+    }   
+       
+    /**
+     * Removes the WP Version as generated by WP
+     */
+    private function disableWPVersion() {
+        remove_action( 'wp_head', 'wp_generator' ); 
+        add_filter( 'the_generator', '__return_null' ); 
+    }  
     
     /**
      * Disables XML RPC. Warning, makes some functions unavailable!
@@ -158,9 +383,6 @@ class Optimize {
         if( is_admin() ) {
             update_option( 'default_ping_status', 'closed' ); // Might do something else here to reduce our queries  
         }
-        
-        // Remove RSD XMLRPC Link
-        remove_action('wp_head', 'rsd_link');  
         
         add_filter( 'xmlrpc_enabled', '__return_false' ); 
         add_filter( 'pre_update_option_enable_xmlrpc', '__return_false' );
@@ -190,7 +412,7 @@ class Optimize {
         }, 10, 1  );
         
     }    
-    
+
     /**
      * Puts jquery inside the footer
      */
@@ -200,117 +422,56 @@ class Optimize {
             wp_register_script( 'jquery', includes_url( '/js/jquery/jquery.js' ), false, NULL, true );
             wp_enqueue_script( 'jquery' );           
         } );    
-    }     
-         
+    } 
+
     /**
-     * Removes WP Emoji
+     * Limits the comment reply JS to the places where it's needed
      */
-    private function removeEmoji() {
-        remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-        remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-        remove_action( 'admin_print_styles', 'print_emoji_styles' );
-        remove_action( 'wp_print_styles', 'print_emoji_styles' );
-        remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-        remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-        remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    private function limitCommentsJS() {
         
-        /**
-         * Removes Emoji from the TinyMCE Editor
-         *
-         * @param array $plugins The plugins hooked onto the TinyMCE Editor
-         */
-        add_filter( 'tiny_mce_plugins', function( $plugins ) {
-            if ( ! is_array($plugins) ) {
-                return array();
-            }
-            return array_diff($plugins, array('wpemoji'));            
-        }, 10, 1 );       
-    }    
-      
+        add_action('wp_print_scripts', function() {
+            if (is_singular() && (get_option('thread_comments') == 1) && comments_open() && have_comments()) {
+                wp_enqueue_script('comment-reply');
+            } else {
+                wp_dequeue_script('comment-reply');
+            }           
+        }, 100);
+
+    }
+    
     /**
-     * Removes links to RSS feeds
+     * Limits post revisions
      */
-    private function removeFeeds() {        
-        remove_action( 'wp_head', 'feed_links_extra', 3 ); 
-        remove_action( 'wp_head', 'feed_links', 2 );   
-        add_action( 'do_feed', array($this, 'removeFeedsHook'), 1 );
-        add_action( 'do_feed_rdf', array($this, 'removeFeedsHook'), 1 );
-        add_action( 'do_feed_rss', array($this, 'removeFeedsHook'), 1 );
-        add_action( 'do_feed_rss2', array($this, 'removeFeedsHook'), 1 );
-        add_action( 'do_feed_atom', array($this, 'removeFeedsHook'), 1 );        
+    private function limitRevisions() {
+
+        if( defined('WP_POST_REVISIONS') && (WP_POST_REVISIONS != false) ) {
+            add_filter( 'wp_revisions_to_keep', function( $num, $post) {
+                return 5;
+            }, 10, 2 );
+        } 
+
+    }
+
+    /**
+     * Removes the styling added to the header for recent comments
+     */
+    private function removeCommentsStyle() {    
+        add_action( 'widgets_init', function() {
+            global $wp_widget_factory;
+            remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
+        });  
     }  
-    
+
     /**
-     * Removes the actual feed links
+     * Slows heartbeat to 1 minute
      */
-    public function removeFeedsHook() {
-        wp_die( '<p>' . __('Feed disabled by WP Optimize.') . '</p>' );
-    }
-    
-    /**
-     * Removes the WP Heartbeat Api. Caution: this disables the autosave functionality 
-     */
-    private function removeHeartbeat() {
-        add_action('admin_enqueue_scripts', function() {
-            wp_deregister_script('heartbeat');    
-        });
-	}
-    
-    /**
-     * Deregisters jQuery.
-     */    
-    private function removejQuery() {
-        add_action( 'wp_enqueue_scripts', function() {
-            wp_deregister_script('jquery');
+    private function slowHeartbeat() {
+        
+        add_filter( 'heartbeat_settings', function($settings) {
+            $settings['interval'] = 60; 
+            return $settings;
         } );
-    }
-    
-    /**
-     * Removes the WP Shortlink 
-     */
-    private function removeShortlinks() { 
-        remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );        
+
     }    
-    
-    /**
-     * Removes the version hook on scripts and styles
-     *
-     * @uses MT_WP_Optimize::no_scripts_styles_version_hook
-     */
-    private function removeVersionNumbers() {
-        add_filter( 'style_loader_src', array($this, 'removeVersionNumbersHook'), 20000 );
-        add_filter( 'script_loader_src', array($this, 'removeVersionNumbersHook'), 20000 ); 
-    }
-    
-    /**
-     * Removes version numbers from scripts and styles. 
-     * The absence of version numbers increases the likelyhood of scripts and styles being cached.
-     *
-     * @param string @target_url The url of the script
-     */
-    public function removeVersionNumbersHook($target_url = '') { 
-        
-        if( strpos( $target_url, 'ver=' ) ) {
-            $target_url = remove_query_arg( 'ver', $target_url );
-        }
-        
-        return $target_url;
-        
-    }
-    
-    /**
-     * Removes WLW manifest bloat
-     */
-    private function removeWLWManifest() {
-        remove_action('wp_head', 'wlwmanifest_link');   
-    }   
-       
-    /**
-     * Removes the WP Version as generated by WP
-     */
-    private function removeWPVersion() {
-        remove_action( 'wp_head', 'wp_generator' ); 
-        add_filter( 'the_generator', '__return_null' ); 
-    }
       
 }
